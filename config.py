1e354ffaa410212272b4f8e9072f9ca32f152089
JaviPeder~ <javierpedernera@gmail.com>
2024-07-23 14:36:54 -0300

    URL

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ class Config:
     # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:javiMono1981@localhost:5432/AppTurismo_DB'
     
     # NEON coneccion
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://AppTurismo_DB_owner:v9GxYRWcKw7j@ep-soft-tree-a5w6w6eq.us-east-2.aws.neon.tech/AppTurismo_DB?sslmode=require'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://AppTurismo_DB_owner:v9GxYRWcKw7j@ep-soft-tree-a5w6w6eq.us-east-2.aws.neon.tech/AppTurismo_DB?sslmode=require'
