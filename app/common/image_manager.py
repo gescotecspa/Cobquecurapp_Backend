07a67913ac3533ce999e1e9bac28184add74a491
pcharras <pablocharras@gmail.com>
2024-08-12 18:26:49 -0300

    Cambios para gestión de imagenes

---
+++ app/common/image_manager.py
@@ -0,0 +1,45 @@
+import base64
+import io
+from google.cloud import storage
+from PIL import Image
+import os
+
+class ImageManager:
+    def __init__(self):
+        self.bucket_name = os.getenv('GCS_BUCKET_NAME', 'turismo-app-cobquecura')
+        self.client = storage.Client()
+        self.bucket = self.client.get_bucket(self.bucket_name)
+
+    def upload_image(self, image_base64, filename):
+        # Decodificar la imagen desde base64
+        try:
+            image_data = base64.b64decode(image_base64)
+        except Exception as e:
+            raise ValueError("Failed to decode Base64 image data") from e
+        
+        # Verificar que los datos decodificados son una imagen válida
+        try:
+            image = Image.open(io.BytesIO(image_data))
+            image.load()  # Carga la imagen en memoria completamente
+        except Exception as e:
+            raise ValueError("Decoded data is not a valid image file") from e
+
+        # Redimensionar la imagen si es necesario
+        resized_image = self.resize_image(image)
+
+        # Subir la imagen redimensionada a Google Cloud Storage
+        blob = self.bucket.blob(filename)
+        output = io.BytesIO()
+        resized_image.save(output, format='PNG')
+        output.seek(0)
+        blob.upload_from_file(output, content_type='image/png')
+
+        # Hacer pública la imagen
+        blob.make_public()
+
+        # Devolver la URL pública de la imagen
+        return blob.public_url
+
+    def resize_image(self, image, max_size=(800, 800)):
+        image.thumbnail(max_size, Image.Resampling.LANCZOS)
+        return image
--- app/models/partner.py
+++ app/models/partner.py
@@ -1,5 +1,5 @@
 from app import db
-
+from .user import User
 # Definición de la tabla de asociación
 partner_categories = db.Table('partner_categories',
     db.Column('partner_user_id', db.Integer, db.ForeignKey('partner_details.user_id'), primary_key=True),
@@ -17,12 +17,20 @@ class Partner(db.Model):
     branches = db.relationship('Branch', back_populates='partner', cascade='all, delete-orphan')  # Añadir esta línea
 
     def serialize(self):
+
+        user = None
+        if self.user_id:
+            user = User.query.get(self.user_id)
+            if user:
+                user = user.serialize()
+
         return {
             "user_id": self.user_id,
             "address": self.address,
             "contact_info": self.contact_info,
             "business_type": self.business_type,
-            "categories": [{"category_id": category.category_id, "name": category.name} for category in self.categories]
+            "categories": [{"category_id": category.category_id, "name": category.name} for category in self.categories],
+            "user": user
         }
 
     def __repr__(self):
