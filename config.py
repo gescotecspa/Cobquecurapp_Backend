3ce3a73e27269fdf06f1f734ba319316548bf978
JaviPeder~ <javierpedernera@gmail.com>
2024-09-16 09:51:02 -0300

    google crednciales

--- config.py
+++ config.py
@@ -17,14 +17,14 @@ class Config:
     GCS_BUCKET_NAME = os.getenv('GCS_BUCKET_NAME', 'turismo-app-cobquecura')
     
     GOOGLE_CREDENTIALS = {
-        "type": os.getenv('GOOGLE_TYPE'),
-        "project_id": os.getenv('GOOGLE_PROJECT_ID'),
-        "private_key_id": os.getenv('GOOGLE_PRIVATE_KEY_ID'),
-        "private_key": os.getenv('GOOGLE_PRIVATE_KEY').replace("\\n", "\n"),
-        "client_email": os.getenv('GOOGLE_CLIENT_EMAIL'),
-        "client_id": os.getenv('GOOGLE_CLIENT_ID'),
-        "auth_uri": os.getenv('GOOGLE_AUTH_URI'),
-        "token_uri": os.getenv('GOOGLE_TOKEN_URI'),
-        "auth_provider_x509_cert_url": os.getenv('GOOGLE_AUTH_PROVIDER_X509_CERT_URL'),
-        "client_x509_cert_url": os.getenv('GOOGLE_CLIENT_X509_CERT_URL')
+        "type": os.getenv('GCP_TYPE'),
+        "project_id": os.getenv('GCP_PROJECT_ID'),
+        "private_key_id": os.getenv('GCP_PRIVATE_KEY_ID'),
+        "private_key": os.getenv('GCP_PRIVATE_KEY').replace("\\n", "\n"),
+        "client_email": os.getenv('GCP_SERVICE_ACCOUNT_EMAIL'),
+        "client_id": os.getenv('GCP_CLIENT_ID'),
+        "auth_uri": os.getenv('GCP_AUTH_URI'),
+        "token_uri": os.getenv('GCP_TOKEN_URI'),
+        "auth_provider_x509_cert_url": os.getenv('GCP_AUTH_PROVIDER_X509_CERT_URL'),
+        "client_x509_cert_url": os.getenv('GCP_CLIENT_X509_CERT_URL')
     }
