db494e580c3ca2fff1de27b517a37c8127040b0d
JaviPeder~ <javierpedernera@gmail.com>
2024-10-29 08:42:58 -0300

    producc

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ app = create_app()
 if __name__ == '__main__':
     # Aquí puedes configurar opciones adicionales para la ejecución del servidor
     # como el puerto, el modo de depuración, etc.
-    app.run(host='0.0.0.0', port=7500, debug=True)  # Cambia debug a False en un entorno de producción
+    app.run(host='0.0.0.0', port=7500, debug=False)  # Cambia debug a False en un entorno de producción
