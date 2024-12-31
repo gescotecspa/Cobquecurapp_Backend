acf0f2bae94a4c01570509583d8cdb3bfe300527
JaviPeder~ <javierpedernera@gmail.com>
2024-12-31 13:35:56 -0300

    status en valoraciones de sucursales

--- app/models/branch_rating.py
+++ app/models/branch_rating.py
@@ -9,9 +9,12 @@ class BranchRating(db.Model):
     branch_id = db.Column(db.Integer, db.ForeignKey('branches.branch_id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('tourists.user_id'), nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=True)
+    status_id = db.Column(db.Integer, db.ForeignKey('statuses.id'), nullable=True)
+    deleted_at = db.Column(db.DateTime, nullable=True) 
     
     branch = db.relationship('Branch', back_populates='ratings')
     tourist = db.relationship('Tourist', backref='tourist_branch_ratings')
+    status = db.relationship('Status', backref='branch_ratings')
 
     def serialize(self, first_name=None):
         return {
@@ -21,5 +24,7 @@ class BranchRating(db.Model):
             'first_name': first_name,
             'rating': self.rating,
             'comment': self.comment,
-            'created_at': self.created_at.isoformat()  # Formatear la fecha en ISO 8601
+            'created_at': self.created_at.isoformat(),
+            'status': self.status.serialize() if self.status else None,
+            'deleted_at': self.deleted_at.isoformat() if self.deleted_at else None 
         }
